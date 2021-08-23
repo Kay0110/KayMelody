6e504b12b3b85abf35a1bdef1f22cef5b54b5a90
Kay0110 <86769448+Kay0110@users.noreply.github.com>
2021-08-23 21:03:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Miu à!',
+    introTitle: 'Chị Lan Anh à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    Mà sao chị cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng chị đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng chị đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h chơi game nha .',
+    mess: 'Anh biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'Chị làm Người yêu Anh nha .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/kay.beo.0110/',
+    messLink: 'https://www.facebook.com/messenger_media/?thread_id=100024338130672&attachment_id=1345908829138468&message_id=mid.%24cAAAAAUYq9b-BpILYnV7cz11nLS52',
 }
